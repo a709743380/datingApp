241ba5d5d3ae8df6588563cdd5968504912d6d71
test <ian>
2025-10-20 22:39:46 +0800

    first commit

---
+++ API/Program.cs
@@ -0,0 +1,21 @@
+using API.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddDbContext<AppDbContext>(opt=>
+{
+    opt.UseSqlite(builder.Configuration.GetConnectionString("Default"));
+});
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+
+var app = builder.Build();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
